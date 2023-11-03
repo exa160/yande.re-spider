92838b49d2f6eabc057c01ebcc7ecb717ea82cd8
exa160 <sfy111@hotmail.com>
2023-11-04 02:45:44 +0800

    add test case

--- unit_test/test_yande_spider.py
+++ unit_test/test_yande_spider.py
@@ -13,6 +13,11 @@ class MyTestCase(unittest.TestCase):
         y.get_post_list()
         # self.assertEqual(True, False)  # add assertion here
 
+    def test_spider_update(self):
+        y = YandeSpider()
+        b_path = r'E:\小工具\python_tool\yande.re_spider\pic'
+        y.update_tags(b_path, [['iijima_masashi', 1127736, False]])
+
 
 if __name__ == '__main__':
     unittest.main()
