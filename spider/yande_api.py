805a923e23d76095fcedce76e874d864517a97cc
exa160 <sfy111@hotmail.com>
2023-11-17 21:54:04 +0800

    增加文件夹内id重复检查

--- spider/yande_api.py
+++ spider/yande_api.py
@@ -51,7 +51,22 @@ class YandeSpider:
         self.search_config = search_config
 
     @staticmethod
-    def item_iter_and_down(yande_item, save_dir_path, get_config):
+    def scan_id_in_dir(save_dir_path) -> set:
+        ret_set = set()
+        for entry_dir in os.scandir(save_dir_path):
+            if not entry_dir.is_file():
+                continue
+            try:
+                yid = entry_dir.name.split(' ', maxsplit=2)[1]
+                yid = int(yid)
+                ret_set.add(yid)
+            except Exception as e:
+                logger.warning(e)
+                continue
+        return ret_set
+
+    @staticmethod
+    def item_iter_and_down(yande_item, save_dir_path, get_config: YandeRunningConfig):
         """
         遍历及下载
         :param yande_item:
@@ -63,6 +78,9 @@ class YandeSpider:
             fn = sanitize_filename(unquote(i.file_url.rsplit("/", maxsplit=1)[-1]))
             if i.id <= get_config.stop_id:
                 return IterStatus.stop
+            if get_config.id_check and get_config.id_check_list is not None:
+                if i.id in get_config.id_check_list:
+                    continue
             if (save_dir_path / fn).exists():
                 if get_config.add_flag:
                     continue
@@ -70,6 +88,9 @@ class YandeSpider:
                     return IterStatus.stop
             MultiDown(i.file_url, str(save_dir_path), fn, i.file_size, i.md5, i.id)
         return IterStatus.next
+
+    def search_trans(self):
+        pass
 
     def get_post_list(self, get_config: YandeRunningConfig = None):
         """
@@ -90,7 +111,7 @@ class YandeSpider:
             save_dir_path = Path('.', tags)
         else:
             save_dir_path = Path(save_dir_path)
-        logger.info(f'*search start\t{"["+tags+"]":>20} \tdown path:{save_dir_path}')
+        logger.info(f'*search start\t{"[" + tags + "]":>20} \tdown path:{save_dir_path}')
         for page in range(s_page, e_page):
 
             if not save_dir_path.exists():
@@ -98,7 +119,7 @@ class YandeSpider:
             # status, yande_item = self.y_api.get_ranking(page, tags='rating:e width:>=10000 ext:png')
             status, yande_item = self.y_api.get_ranking(page, tags=tags)
             if len(yande_item.root) == 0:
-                logger.info(f'**search finish\t{"["+tags+"]":>20}')
+                logger.info(f'**search finish\t{"[" + tags + "]":>20}')
                 break
             iter_status = self.item_iter_and_down(yande_item, save_dir_path, get_config)
             if iter_status == IterStatus.stop:
@@ -116,5 +137,7 @@ class YandeSpider:
             get_config.tags = tag
             get_config.stop_id = stop_id
             get_config.save_dir_path = Path(b_path) / tag
+            if get_config.id_check:
+                get_config.id_check_list = YandeSpider.scan_id_in_dir(get_config.save_dir_path)
             get_config.add_flag = add_flag
             self.get_post_list(get_config)
